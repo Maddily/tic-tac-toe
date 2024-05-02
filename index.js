94046d7323705e6f28e9e9825adc0101530de411
Maddie <115709272+Maddily@users.noreply.github.com>
2024-05-02 12:27:13 +0300

    Create game board object

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Create a gameboard object
+  const gameBoard = (function () {
+    let board = ['', '', '', '', '', '', '', '', ''];
+
+    return {board};
+  })();
+});
